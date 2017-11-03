b0f764632287fb20c3690f127cd78f6b095d9693
Chris-Desktop <chrisshyi13@gmail.com>
2017-11-03 17:11:37 -0400

    Created models Question and Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 300)
+    pub_date = models.DateField('date published')
+    # voters is a JSON serialized Python list of strings
+    voters = models.CharField(max_length = 300)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # The fields below pertain to the restaurant/venue this choice is associated with
+    venue_name = models.CharField(max_length = 150)
+    venue_category = models.CharField(max_length = 200)
+    venue_picture_url = models.CharField(max_length = 250)
+    avg_rating = models.SmallPositiveIntegerField()
+    yelp_page_url = models.CharField(max_length = 250)
+    # End of venue fields
+    votes = models.SmallPositiveIntegerField(default = 0)

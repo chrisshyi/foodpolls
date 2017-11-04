038f133b846322a4b18a0128df680155f4808a06
Chris-Desktop <chrisshyi13@gmail.com>
2017-11-04 13:38:30 -0400

    Added creator_email field to Question

--- polls/models.py
+++ polls/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length = 300)
+    question_text = models.CharField(max_length = 60)
     pub_date = models.DateField('date published')
     # voters is a JSON serialized Python list of strings
-    voters = models.CharField(max_length = 300)
+    voters = models.CharField(max_length = 200)
+    creator_email = models.EmailField(default = '')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)

be6c8757a994e418ef500486ed4e56cb0fa1426b
Chris <chrisshyi13@gmail.com>
2018-02-03 21:38:59 -0500

    Fixed venue display issue
    The venues list on the poll display page was too tall, resulting in some of the content at the bottom being cut off. This is now fixed. A little bit of JavaScript is added to change the height when the poll id is displayed.

--- polls/static/polls/scripts/javascript/poll_display.js
+++ polls/static/polls/scripts/javascript/poll_display.js
@@ -6,9 +6,12 @@ document.getElementById("invite-btn").addEventListener("click", function() {
     let inviteBtn = document.getElementById("invite-btn");
     inviteBtn.classList.toggle("btn-primary");
     inviteBtn.classList.toggle("btn-danger");
+    let venueListings = document.getElementById("venues-list");
     if (inviteBtn.classList.contains("btn-danger")) {
         inviteBtn.innerText = "Hide";
+        venueListings.style.height = "57vh"
     } else {
         inviteBtn.innerText = "Invite Friends";
+        venueListings.style.height = "66vh";
     }
 });

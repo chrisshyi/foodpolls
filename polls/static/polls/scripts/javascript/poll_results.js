b3ac4b82c03be4628b8ffd27d8282cf64c332b83
Chris Shyi <chris.shyi@mail.utoronto.ca>
2018-02-11 16:33:27 -0500

    Added event handler to "Invite Friends" button for poll results

--- polls/static/polls/scripts/javascript/poll_results.js
+++ polls/static/polls/scripts/javascript/poll_results.js
@@ -1,6 +1,25 @@
 /* Set up variables needed for AJAX */
 let csrfToken = Cookies.get('csrftoken');
 
+/* Add an event handler to the "Invite Friends" button so that the poll id is displayed */
+document.getElementById("invite-btn").addEventListener("click", () => {
+    let idDisplayRow = document.getElementById("poll-id-display-row");
+    idDisplayRow.classList.toggle("id-display-hidden");
+    idDisplayRow.classList.toggle("id-display-shown");
+
+    let inviteBtn = document.getElementById("invite-btn");
+    inviteBtn.classList.toggle("btn-primary");
+    inviteBtn.classList.toggle("btn-danger");
+    let venueListings = document.getElementById("venues-list");
+    if (inviteBtn.classList.contains("btn-danger")) {
+        inviteBtn.innerText = "Hide";
+        venueListings.style.height = "57vh"
+    } else {
+        inviteBtn.innerText = "Invite Friends";
+        venueListings.style.height = "66vh";
+    }
+});
+
 let voterLinks = document.getElementsByClassName("voters-link");
 for (let voterLink of voterLinks) {
     voterLink.addEventListener("click", function() {
